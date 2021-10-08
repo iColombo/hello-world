0aeb22ae995295bb4772db99f985b402a7e583f9
iColombo <ivandrocolombo86@gmail.com>
2021-10-08 12:05:52 +0100

    Bem-Vindo ao JAVA+JSF+PrimeFaces

---
+++ src/main/java/com/sadam/hello/world/jsf/JAXRSConfiguration.java
@@ -0,0 +1,13 @@
+package com.sadam.hello.world.jsf;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
